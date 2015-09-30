840cb9597833c2fecb705ebeba8ad9a8d1c6536b
lester88a <lester88a@gmail.com>
2015-09-30 09:26:24 -0400

    Searching for a key

--- HashTableDictionary/Program.cs
+++ HashTableDictionary/Program.cs
@@ -37,13 +37,36 @@ namespace HashTableDictionary
              *  1.Return null if you try to access a key which does not exist.
              *  2.Slower than dictionary because it requires boxing and unboxing
              *  3.Non-generic
-             */ 
-
+             */
+            hashtable.Add(1, "One");
+            hashtable.Add(2, "Two");
+            hashtable.Add(10, "Ten");
+            for (int i = 0; i < hashtable.Count; i++)
+            {
+                Console.WriteLine(hashtable[i]);//output> One and Two, hashtable[0]=null, no error
+            }
             /*  Dictionary
              *  1. return error for the same situation
              *  2. Faster than hashtable, no boxing, unboxing behind the scene.
              *  3. Generic
              */
+            Dictionary<int, string> dictionaries = new Dictionary<int, string>();
+            dictionaries.Add(1, "One");
+            dictionaries.Add(2, "Two");
+            dictionaries.Add(3, "Ten");
+            for (int i = 0; i < dictionaries.Count; i++)
+            {
+                Console.WriteLine(dictionaries[i]);//output> Error due to dictionaries[0] cannot empty
+            }
+
+            //Searching for a key
+            if (dictionary.ContainsKey("Cat")==true)
+            {
+                //doing something
+            }
+
+            //Hashtable example part 1
+
         }
     }
 }
