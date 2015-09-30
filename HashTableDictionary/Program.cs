788c17fdb462f560ef41b42ee9981e75654fdf54
lester88a <lester88a@gmail.com>
2015-09-30 08:53:33 -0400

    Basic

---
+++ HashTableDictionary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*                                 Collections
+             * 
+             * Hash Table > Non-Generic[not type safe] > using system.collections;
+             * Dictionary > generic >[type-safe] > using system.collections.generic;
+             * 
+             * Hash Table & Dictionary are collections > they need (key, value) perimeter
+             */
+            //Hash table
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add("Area", 1000);
+            hashtable.Add("Perimeter", 55);
+            int value = (int)hashtable["Area"];
+            hashtable.Add(55, "li xu");
+
+            //Dictionary
+
+        }
+    }
+}
