b46881b08ca7aeb2beffba2f3eb7e302b3bdd140
lester88a <lester88a@gmail.com>
2015-09-30 08:58:53 -0400

    Dictionary

--- HashTableDictionary/Program.cs
+++ HashTableDictionary/Program.cs
@@ -26,7 +26,12 @@ namespace HashTableDictionary
             hashtable.Add(55, "li xu");
 
             //Dictionary
-
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            dictionary.Add("Cat", 1);
+            dictionary.Add("Dog", 2);
+            dictionary.Add("Icama", 0);
+            dictionary.Add("Iguana", -1);
+            //dictionary.Add(1, -2); //not allowed
         }
     }
 }
